d061866bf2c41ea15ea217f672b539e2a0b23f1c
Rahul Sharma <rahultaipan1@.com>
2020-05-15 13:15:01 +0530

    This Includes the blog app but cannot move from hometo blog via url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import BlogProject
+def all_blogs(request):
+    blog_projects = BlogProject.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blog_projects':blog_projects})
