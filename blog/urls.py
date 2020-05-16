e389805c1f9603cca7e34ff7a6b0c48984654b02
Rahul Sharma <rahultaipan1@.com>
2020-05-16 10:36:40 +0530

    Before Polish

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from blog import views
+
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
     path('<int:blog_id>/', views.detail, name = 'detail'),
